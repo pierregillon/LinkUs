0e4e091fe3dd7045e9edc64c4373e2dcf1f1736e
Pierre GILLON <gillon.pierre69@gmail.com>
2017-11-18 18:47:55 +0900

    Update Ping to support delay parameter.

--- LinkUs.CommandLine/Handlers/PingCommandLineHandler.cs
+++ LinkUs.CommandLine/Handlers/PingCommandLineHandler.cs
@@ -20,15 +20,15 @@ namespace LinkUs.CommandLine.Handlers
         public async Task Handle(PingCommandLine commandLine)
         {
             var client = await _server.FindRemoteClient(commandLine.Target);
-            _console.NewLine();
             for (var requestNumber = 1; requestNumber <= commandLine.RequestCount; requestNumber++) {
                 _console.Write($"* {requestNumber}. Send ping request to {client.Information.MachineName} ... ");
                 var pingEllapsedTime = await client.Ping();
                 _console.WriteLine($"[OK] {pingEllapsedTime}ms.");
                 if (requestNumber != commandLine.RequestCount) {
-                    Thread.Sleep(1000);
+                    Thread.Sleep(commandLine.DelayMs);
                 }
             }
+            _console.NewLine();
         }
     }
 }
--- LinkUs.CommandLine/Verbs/PingCommandLine.cs
+++ LinkUs.CommandLine/Verbs/PingCommandLine.cs
@@ -6,5 +6,8 @@ namespace LinkUs.CommandLine.Verbs
     {
         [Option('c', "count", Required = false, DefaultValue = 5, HelpText = "The number of request to send.")]
         public int RequestCount { get; set; }
+
+        [Option('d', "delay", Required = false, DefaultValue = 300, HelpText = "Delay in milliseconds between two ping requests.")]
+        public int DelayMs { get; set; }
     }
 }
